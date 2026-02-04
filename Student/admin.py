846da910912b93d7663ab54a6304a351078cb985
Dhruvil Ramolia <146636624+Dhruvil4625@users.noreply.github.com>
2026-02-04 12:08:57 +0530

    Register student, Product, and MarketingCampaign models

--- Student/admin.py
+++ Student/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import MarketingCampaign, student,Product
 # Register your models here.
+
+admin.site.register(student)
+admin.site.register(Product)
+admin.site.register(MarketingCampaign)
