58425cf8f4e3c83f950ece4058af82ed81bb4d01
Dhruvil Ramolia <dkrramolia20@gmail.com>
2026-02-06 14:09:57 +0530

    Updated admin file for today task

--- Student/admin.py
+++ Student/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
-from .models import MarketingCampaign, student,Product
+from .models import MarketingCampaign, Student, Product, Category, StudentProfile, UserAccount,UserProfile,Course,Enrollment,AdCreative,Lead
+
 # Register your models here.
 
-admin.site.register(student)
+admin.site.register(Student)
 admin.site.register(Product)
 admin.site.register(MarketingCampaign)
+admin.site.register(StudentProfile)
+admin.site.register(Category)
+admin.site.register( UserAccount)
+admin.site.register(UserProfile)
+admin.site.register(Course)
+admin.site.register(Enrollment)
+admin.site.register(AdCreative)
+admin.site.register(Lead)
+
